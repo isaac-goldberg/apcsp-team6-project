37f5b66b0b9823492a7e0bfb14b2a455edae83ce
isaac-goldberg <isaacg0ldb3rg@gmail.com>
2023-11-30 15:57:36 -0800

    refactor: planet and moon initialization

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import turtle as trtl
-import math
+from math import sin, cos, radians
+
+SUN_RADIUS = 50
+MAX_ORBITS = 100
+
+simulation_speed = input("Enter the speed (integer) to run the simulation at: ")
+while type(simulation_speed) is not int:
+    try:
+        simulation_speed = int(simulation_speed)
+    except:
+        simulation_speed = input("Invalid input, must be an integer. Try again: ")
 
 wn = trtl.Screen()
 wn.bgcolor("black")
-
-SUN_RADIUS = 50
-EARTH_DISTANCE_FROM_SUN = 200
-MOON_DISTANCE_FROM_EARTH = 45
-TOTAL_REVOLUTIONS = 2
 
 # initialize sun turtle
 sun = trtl.Turtle()
@@ -22,35 +27,48 @@ sun.begin_fill()
 sun.circle(radius=SUN_RADIUS, steps=64)
 sun.end_fill()
 
-# initialize earth turtle
-earth = trtl.Turtle(shape="circle")
-earth.hideturtle()
-earth.speed(0)
-earth.penup()
-earth.shapesize(1.5)
-earth.color("green")
+# planet name, turtle object, color, distance from sun, size, speed, moons list
+# for each moon: turtle object, distance from planet, size, speed
+planets = [
+    ["mercury", None, "#824a42", 80, 0.6, 3.5, []],
+    ["venus", None, "red", 120, 1.4, 1.5, []],
+    ["earth", None, "green", 175, 2, 1, [[None, 40, 0.75, 3]]],
+    ["mars", None, "orange", 275, 1.2, 0.5, [[None, 30, 0.8, 4], [None, 50, 0.6, 2]]]
+]
 
-# initialize moon turtle
-moon = trtl.Turtle(shape="circle")
-moon.hideturtle()
-moon.speed(0)
-moon.penup()
-moon.shapesize(0.75)
-moon.color("grey")
+for planet in planets:
+    p = trtl.Turtle(shape="circle")
+    p.hideturtle()
+    p.speed(0)
+    p.penup()
+    p.shapesize(planet[4])
+    p.color(planet[2])
+    planet[1] = p
 
-for i in range(360 * TOTAL_REVOLUTIONS):
-    ex = EARTH_DISTANCE_FROM_SUN * math.sin(math.radians(i))
-    ey = EARTH_DISTANCE_FROM_SUN * math.cos(math.radians(i))
-    earth.goto(ex, ey)
+    for moon in planet[6]:
+        m = trtl.Turtle(shape="circle")
+        m.hideturtle()
+        m.speed(0)
+        m.penup()
+        m.shapesize(moon[2])
+        m.color("gray")
+        moon[0] = m
 
-    mx = MOON_DISTANCE_FROM_EARTH * math.sin(math.radians(i * 2.75)) + earth.xcor()
-    my = MOON_DISTANCE_FROM_EARTH * math.cos(math.radians(i * 2.75)) + earth.ycor()
-    moon.goto(mx, my)
+for i in range(360 * MAX_ORBITS):
+    angle = i * simulation_speed
+    for planet in planets:
+        x = planet[3] * sin(radians(angle * planet[5]))
+        y = planet[3] * cos(radians(angle * planet[5]))
+        planet[1].goto(x, y)
 
-    if i == 0:
-        earth.showturtle()
-        earth.speed(5)
-        moon.showturtle()
-        moon.speed(5)
+        for moon in planet[6]:
+            mx = moon[1] * sin(radians(angle * moon[3])) + planet[1].xcor()
+            my = moon[1] * cos(radians(angle * moon[3])) + planet[1].ycor()
+            moon[0].goto(mx, my)
+
+            if angle == 0:
+                moon[0].showturtle()
+        if angle == 0:
+            planet[1].showturtle()
 
 wn.mainloop()
