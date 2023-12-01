49fabe7dbb43128eedf304bb4b79c40279f74428
isaac-goldberg <isaacg0ldb3rg@gmail.com>
2023-11-30 16:12:29 -0800

    doc: commented the entire program

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import turtle as trtl
 from math import sin, cos, radians
 
+# constants
 SUN_RADIUS = 50
 MAX_ORBITS = 100
 
+# prompt the user for an animation speed,
+# and keep asking them until they give a valid integer input
 simulation_speed = input("Enter the speed (integer) to run the simulation at: ")
 while type(simulation_speed) is not int:
     try:
@@ -11,19 +14,31 @@ while type(simulation_speed) is not int:
     except:
         simulation_speed = input("Invalid input, must be an integer. Try again: ")
 
-wn = trtl.Screen()
-wn.bgcolor("black")
+# the screen that will be drawn on - initialize it here so that we can
+# give the entire screen a black background before rendering anything else
+screen = trtl.Screen()
+screen.bgcolor("black")
 
-# initialize sun turtle
+# turtle that will write the loading screen text
+writer = trtl.Turtle()
+writer.speed(0)
+writer.hideturtle()
+writer.penup()
+writer.goto(0, -300)
+writer.color("white")
+writer.pendown()
+writer.write("Loading...", align="center", font=("Arial", 24, "normal"))
+
+# initialize sun turtle,
+# and draw the sun so that its center is in the exact center of the screen
 sun = trtl.Turtle()
 sun.hideturtle()
 sun.speed(0)
 sun.color("yellow")
 sun.penup()
-# draw the sun so that the center is exactly in the center of the screen
 sun.goto(0, -SUN_RADIUS)
 sun.pendown()
-sun.begin_fill()
+sun.begin_fill() # this will fill in the circle, instead of just being an outline
 sun.circle(radius=SUN_RADIUS, steps=64)
 sun.end_fill()
 
@@ -36,6 +51,7 @@ planets = [
     ["mars", None, "orange", 275, 1.2, 0.5, [[None, 30, 0.8, 4], [None, 50, 0.6, 2]]]
 ]
 
+# initialize the turtle objects for each planet
 for planet in planets:
     p = trtl.Turtle(shape="circle")
     p.hideturtle()
@@ -45,6 +61,7 @@ for planet in planets:
     p.color(planet[2])
     planet[1] = p
 
+    # initialize the turtle objects for each moon
     for moon in planet[6]:
         m = trtl.Turtle(shape="circle")
         m.hideturtle()
@@ -54,21 +71,32 @@ for planet in planets:
         m.color("gray")
         moon[0] = m
 
+# main loop that will go until we have orbited MAX_ORBITS times
 for i in range(360 * MAX_ORBITS):
-    angle = i * simulation_speed
+    angle = i * simulation_speed # changes the speed of the objects
+
+    # for each planet, move to position calculated from current angle, with the sun as the center
+    # using trig functions (FYI, python math only accepts radians, not degrees)
     for planet in planets:
         x = planet[3] * sin(radians(angle * planet[5]))
         y = planet[3] * cos(radians(angle * planet[5]))
         planet[1].goto(x, y)
 
+        # for each moon of the planet (if any),
+        # move to position calculated from angle, with the planet as the center
         for moon in planet[6]:
             mx = moon[1] * sin(radians(angle * moon[3])) + planet[1].xcor()
             my = moon[1] * cos(radians(angle * moon[3])) + planet[1].ycor()
             moon[0].goto(mx, my)
 
-            if angle == 0:
+            # if this is the first iteration, reveal all the planets and moons
+            if i == 0:
                 moon[0].showturtle()
-        if angle == 0:
+        if i == 0:
             planet[1].showturtle()
+    # also delete the loading text if this is the first iteration
+    if i == 0:
+        writer.clear()
 
-wn.mainloop()
+# start screen updates
+screen.mainloop()
